793c21fec6ddf58fa9a188e356b7d2d1019c7053
archi <apoczek@gmail.com>
2020-04-03 10:14:45 +0200

    Add recommendation system
    - modify view and detail.html so now posts with same tags are recommended

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 from.models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
@@ -51,11 +52,15 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    simmilar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    simmilar_posts = simmilar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'simmilar_posts': simmilar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
